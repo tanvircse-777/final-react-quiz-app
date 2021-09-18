cf03825eeb157b74b01e86230cfdaac4c0681afe
Tanvir <tanvircse777@gmail.com>
2021-09-18 19:59:32 +0600

    'quiz-page-loading'

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCKXBBBX4xAYm-AWg0NSfl1gUPBl25MVoM",
-  authDomain: "my-react-app-dev-d7686.firebaseapp.com",
-  databaseURL:
-    "https://my-react-app-dev-d7686-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "my-react-app-dev-d7686",
-  storageBucket: "my-react-app-dev-d7686.appspot.com",
-  messagingSenderId: "547168710919",
-  appId: "1:547168710919:web:8be98fe60b461ee96555a2",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
